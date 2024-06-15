b6f85a7a4fcdc95d1f2b35c5fffa77c75351d2e7
saadiqbal-dev <saadiqbal.dev@outlook.com>
2024-06-16 00:12:20 +0500

    Button Component

---
+++ widget/page/welcome/Index.jsx
@@ -0,0 +1,41 @@
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  width: 100%;
+`;
+
+const Title = styled.h1`
+  font-size: 2rem;
+  margin-bottom: 1rem;
+`;
+
+const Subtitle = styled.h2`
+  font-size: 1.5rem;
+  margin-bottom: 2rem;
+`;
+
+const Button = styled.button`
+  background-color: #4CAF50;
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+`;
+
+const Index = () => (
+  <Container>
+    <Title>Welcome to the</Title>
+    <Subtitle>React App</Subtitle>
+    <Button onClick={() => window.location.reload()}>Reload</Button>
+  </Container>
+);
+
+return <Index />;
