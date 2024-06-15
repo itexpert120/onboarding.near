10d815f5650a52ff1921f79115c44576d500eeb3
saadiqbal-dev <saadiqbal.dev@outlook.com>
2024-06-16 02:24:50 +0500

    Profile paeg

--- widget/page/profileInfo/Profile.jsx
+++ widget/page/profileInfo/Profile.jsx
@@ -1,1 +1,156 @@
-return <div>Profile</div>;
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+  justify-content: center;
+  gap: 32px;
+
+  .heading {
+    color: var(--profile-heading-color, #171717);
+    text-align: center;
+    font-size: 28px;
+    letter-spacing: -1.12px;
+    margin: 0px;
+  }
+`;
+
+const FormGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+  justify-content: center;
+  gap: 16px;
+
+  .form-group {
+    gap: 6px;
+    display: flex;
+    flex-direction: column;
+
+    label {
+      color: var(--Gray-12, #171717);
+
+      /* Text/S - 14px/Medium */
+
+      font-size: 14px;
+      font-style: normal;
+      font-weight: 500;
+      line-height: 140%; /* 19.6px */
+      letter-spacing: -0.14px;
+    }
+    input,
+    textarea {
+      border-radius: 12px !important;
+      border: 1px solid var(--Gray-6, #e2e2e2) !important;
+      background: var(--BG-Main, #fff) !important;
+    }
+  }
+
+  .rbt-input-multi.form-control {
+    border-radius: 12px !important;
+    border: 1px solid var(--Gray-6, #e2e2e2) !important;
+    background: var(--BG-Main, #fff) !important;
+    padding: 8px;
+  }
+`;
+
+const [formState, setFormState] = useState({});
+const onInputChange = (e) => {
+  setFormState((prevState) => ({
+    ...prevState,
+    [e.target.id]: e.target.value,
+  }));
+};
+
+useEffect(() => {
+  console.log(formState);
+}, [formState]);
+
+return (
+  <Container>
+    <h1 className="heading">Tell us about yourself</h1>
+    <FormGroup>
+      <div className="form-group">
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          class="form-control"
+          name="name"
+          id="name"
+          value={formState.name}
+          onChange={onInputChange}
+          placeholder="Your preferred name"
+        ></input>
+      </div>
+      <div className="form-group">
+        <label htmlFor="name">About</label>
+        <small id="emailHelp" class="form-text text-muted">
+          Supports Markdown
+        </small>
+        <textarea
+          type="text"
+          class="form-control"
+          id="about"
+          onChange={onInputChange}
+          placeholder="Your preferred name"
+        ></textarea>
+        <small id="emailHelp" class="form-text text-muted">
+          --words count
+        </small>
+      </div>
+      <div>
+        <div className="form-group mb-2">
+          <label htmlFor="name">Tags</label>
+        </div>
+        <Typeahead
+          options={["asd", "dsa"]}
+          placeholder="Select one or many"
+          multiple
+          selected={formState.tags}
+          onChange={(e) => setFormState({ ...formState, tags: e })}
+          allowNew={true}
+          style={{ width: "100%" }}
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="name">Twitter</label>
+        <input
+          type="text"
+          class="form-control"
+          id="twitter"
+          onChange={onInputChange}
+          placeholder="twitter.com/ username"
+        ></input>
+      </div>
+      <div className="form-group">
+        <label htmlFor="name">Github</label>
+        <input
+          type="text"
+          id="github"
+          onChange={onInputChange}
+          class="form-control"
+          placeholder="github.com/ username"
+        ></input>
+      </div>
+      <div className="form-group">
+        <label htmlFor="name">Telegram</label>
+        <input
+          type="text"
+          id="telegram"
+          onChange={onInputChange}
+          class="form-control"
+          placeholder="t.me/ username"
+        ></input>
+      </div>
+      <div className="form-group">
+        <label htmlFor="name">Website</label>
+        <input
+          type="text"
+          id="website"
+          onChange={onInputChange}
+          class="form-control"
+          placeholder="https:// website"
+        ></input>
+      </div>
+    </FormGroup>
+  </Container>
+);
