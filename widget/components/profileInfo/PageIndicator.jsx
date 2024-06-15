9a4b76e951885010615623f86da0ff542363a84a
Zeeshan Ahmad <itexpert120@outlook.com>
2024-06-16 01:54:47 +0500

    Update Page Indicator

--- widget/components/profileInfo/PageIndicator.jsx
+++ widget/components/profileInfo/PageIndicator.jsx
@@ -31,58 +31,81 @@ const PageIndicator = styled.div`
     transition: all 0.2s ease-in-out;
     position: relative;
 
+    &::before {
+      content: "";
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 1px;
+      background-color: #e2e2e2;
+    }
+
     &::after {
       content: "";
       position: absolute;
       bottom: 100%;
       left: 50%;
-      transform: translate(-50%, 50%);
       width: 12px;
       height: 12px;
       border-radius: 50%;
       background-color: #e2e2e2;
+      transform: translate(-50%, 50%);
+      transition: all 0.2s ease-in-out;
+      z-index: 2;
     }
 
     &.active {
       &::after {
         background-color: #000;
+        width: 18px;
+        height: 18px;
+        border: 4px solid #e2e2e2;
+      }
+    }
+
+    &.completed {
+      &::after {
+        background-color: #299764;
+        width: 16px;
+        height: 16px;
+        border: 2px solid #299764;
       }
 
       &::before {
-        content: "";
-        position: absolute;
-        bottom: 100%;
-        left: 50%;
-        transform: translate(-50%, 50%);
-        width: 18px;
-        height: 18px;
-        border-radius: 50%;
-        background-color: #e2e2e2;
+        background-color: #299764;
+        z-index: 1;
+        transform: translateX(calc(100% - 45px));
       }
+    }
+  }
+
+  .item:first-child {
+    &::before {
+      transform: translateX(50%);
+    }
+  }
+
+  .item:last-child {
+    &::before {
+      transform: translateX(-50%);
     }
   }
 `;
 
-const IndicatorLine = styled.div`
-  position: absolute;
-  top: 0;
-  left: 50%;
-  transform: translateX(-50%);
-  width: calc(100% - 128px);
-  height: 1px;
-  background-color: #e2e2e2;
-`;
-
 return (
   <PageIndicator>
-    <IndicatorLine />
     {pages.map((page) => {
       const pageKey = getPageKey(page);
       const isActive = pageKey === getPageKey(currentPage);
       return (
         <div
           key={pageKey}
-          className={`item ${isActive ? "active" : ""}`}
+          className={`item ${isActive ? "active" : null} ${
+            pages.indexOf(page) < pages.indexOf(currentPage)
+              ? "completed"
+              : null
+          }`}
           onClick={() => props.setCurrentPage(page)}
         >
           {pageKey}
