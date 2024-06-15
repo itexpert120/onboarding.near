1b7776f53e1588be711ae2354a3c4005800a68e8
Zeeshan Ahmad <itexpert120@outlook.com>
2024-06-15 23:48:20 +0500

    Initial Commit

---
+++ widget/Index.jsx
@@ -0,0 +1,34 @@
+const config = {
+  theme: {},
+  layout: {
+    src: "devs.near/widget/Layout",
+    props: {
+      variant: "standard",
+    },
+  },
+  blocks: {
+    Header: () => <></>,
+    Footer: () => <></>,
+  },
+  router: {
+    param: "page",
+    routes: {
+      home: {
+        path: "${config_account}/widget/page.home.Index",
+        blockHeight: "final",
+        init: {
+          name: "Home",
+        },
+        default: true,
+      },
+    },
+  },
+};
+return (
+  <div>
+    <Widget
+      src="${config_account}/widget/app.View"
+      props={{ config, ...props }}
+    />
+  </div>
+);
